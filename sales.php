7f6b3e606b07af67a2f6a12223455788b1f7003e
Shikina Cabral <130032168+CShikina@users.noreply.github.com>
2025-06-22 14:38:42 +0800

    feat: transaction logs page

--- sales.php
+++ sales.php
@@ -64,8 +64,9 @@ if ($result && $result->num_rows > 0) {
   <section>
     <nav class="admin-navbar">
       <ul>
-        <li><a href="recordManagement.php">Most Travelled Route</a></li>
+        <li><a href="history.php">Most Travelled Route</a></li>
         <li><a href="sales.php">Sales</a></li>
+        <li><a href="transactionLogs.php">Transaction Logs</a></li>
       </ul>
     </nav>
   </section>
